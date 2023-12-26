a29eddc4e21b7fe7567c1bdaa077796d4f767f5c
Yiwei Gong <imwithye@gmail.com>
2023-12-26 10:50:19 +0800

    update setup-vnc

--- setup-vncserver.sh
+++ setup-vncserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install -y libglu1-mesa-dev mesa-utils xterm xauth x11-xkb-utils xfonts-base xkb-data libxtst6 libxv1 icewm gsettings-desktop-schemas
+apt update && apt install -y wget libglu1-mesa-dev mesa-utils xterm xauth x11-xkb-utils xfonts-base xkb-data libxtst6 libxv1 icewm gsettings-desktop-schemas
 
 export TURBOVNC_VERSION=2.2.5
 export LIBJPEG_VERSION=2.0.90
